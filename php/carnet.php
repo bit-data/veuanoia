c59dd22ed3bd235d360876bf5e93576a0c6db997
albertescobar <albert.escobar@bit-data.es>
2022-12-05 17:24:21 +0100

    creació carnet capes

---
+++ php/carnet.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>index</title>
+<link rel="stylesheet" type="text/css" href="../css/styles.css">
+
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+</head>
+<body>
+  <div id="logo">
+    <img src="../images/logo_sub2.png" alt="Logo del club del subscriptor" width="30%">
+  </div>
+  <div id="main">
+    <div id="carnet">
+      <div id="telefon">
+        <p>Telèfon d'antenció al subscriptor</p>
+        <p>93 804 24 51/<p>
+      </div>
+        <div id="usuari">
+          <p>Albert Escobar Ameijeiras</p>
+        </div>
+    </div>
+  </div>
+  <footer>
+  Politica privacitat, cookies, desenvolupador
+  </footer>
+</body>
+</html>
--- php/menu.php
+++ php/menu.php
@@ -14,7 +14,7 @@
     <h1>Hola <?php echo $_COOKIE['nom'] ?>!</h1>
 
     <a href="carnet.php"><button class="bt_mida">El meu carnet</button></a>
-    <a href="carnet.php"><button class="bt_mida">Promocions</button></a>
+    <a href="https://veuanoia.cat/"><button class="bt_mida">Promocions</button></a>
     <a href="contacte.php"><button class="bt_mida">Contacte</button></a>
     <form method="post">
         <input type="submit" name="button1" class="bt_mida" value="Tancar sessió" />
