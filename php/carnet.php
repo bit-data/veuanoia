15c146cf9cfc91756f2b4aa0442b313f45586d19
aescobara <albert.escobar@bit-data.es>
2022-12-06 00:23:55 +0100

    capes carnet

--- php/carnet.php
+++ php/carnet.php
@@ -13,11 +13,19 @@
   <div id="main">
     <div id="carnet">
       <div id="telefon">
-        <p>Telèfon d'antenció al subscriptor</p>
-        <p>93 804 24 51/<p>
+        <p><h2>Telèfon d'antenció al subscriptor</h2</p>
+        <h2>93 804 24 51/<h2>
       </div>
         <div id="usuari">
           <p>Albert Escobar Ameijeiras</p>
+          <p>Nº Subscriptor/a</p>
+        </div>
+        <div id="info">
+          <p>Aquesta targeta serveix per gaudir de tots els avantatges i
+            descomptes destinats als subscriptors. En poden gaudir també
+            familiars directes del titular.
+            En cas de pèrdua cal comunicar-ho al departament de subscriptors.</p>
+            <h1>www.veuanoia.cat</h1>
         </div>
     </div>
   </div>
