c66ba220a2231418040612b1b36c48e8ed6dd021
aescobara <albert.escobar@bit-data.es>
2022-12-21 00:27:04 +0100

    1/2 footer

--- index_2.php
+++ index_2.php
@@ -85,11 +85,34 @@
     </label>
   </div>
   <button class="w-100 btn btn-lg btn-primary" type="submit" name="login" value="login">Entrar</button>
-  <p class="mt-5 mb-3 text-muted">&copy; 2017–2022</p>
+  </div>
 </form>
+<div class="container">
+  <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+    <div class="col-md-4 d-flex align-items-center">
+      <span class="mb-3 mb-md-0 text-muted"><ul>
+        <li>&copy; 2022 La Veu Anoia</li>
+        <li><a href="">Política de privacitat</li></a>
+        <li><a href="">Política de cookies</li></a>
+        <li>Developed by <a href="https://wwww.bit-data.es">Bit_Data</li></a>
+      </ul></span>
+    </div>
+
+    <ul class="">
+      <li class="ms-3"><a class="text-muted" href="https://twitter.com/veuanoia"><img src="images/xs/twitter.svg" class="bi" width="24" height="24"></a></li>
+      <li class="ms-3"><a class="text-muted" href="https://www.instagram.com/veuanoia/"><img src="images/xs/instagram.svg" class="bi" width="24" height="24"></a></li>
+      <li class="ms-3"><a class="text-muted" href="https://www.facebook.com/VeuAnoia/"><img src="images/xs/facebook.svg" class="bi" width="24" height="24"></a></li>
+      <li class="ms-3"><a class="text-muted" href=""><img src="images/xs/linkedin.svg" class="bi" width="24" height="24"></a></li>
+      <li class="ms-3"><a class="text-muted" href="https://www.youtube.com/"><img src="images/xs/youtube.svg" class="bi" width="24" height="24"></a></li>
+    </ul>
+  </footer>
 </main>
 
+
+
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
+
 
 </body>
 </html>
