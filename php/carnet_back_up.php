670d3bb3582f8c5ba9538d6e413d777bdaa76767
aescobara <albert.escobar@bit-data.es>
2022-12-23 22:01:37 +0100

    Error login i css

---
+++ php/carnet_back_up.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>index</title>
+<link rel="stylesheet" type="text/css" href="../css/styles.css">
+
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+</head>
+<body>
+  <div id="logo">
+    <img src="../images/logo_sub2.png" alt="Logo del club del subscriptor" width="30%">
+  </div>
+  <div id="main">
+    <div id="carnet">
+      <div id="telefon">
+        <h2>Telèfon d'antenció al subscriptor</br>
+        93 804 24 51 / hola@email.cat<h2>
+      </div>
+        <div id="usuari">
+          <p><?php echo $_COOKIE['nom']." ".$_COOKIE['cognoms'] ?></br>
+          nº subscriptor/a: <?php echo $_COOKIE['subscriptor'] ?></p>
+        </div>
+        <div id="info">
+          <p>Aquesta targeta serveix per gaudir de tots els avantatges i
+            descomptes destinats als subscriptors. En poden gaudir també
+            familiars directes del titular.
+            En cas de pèrdua cal comunicar-ho al departament de subscriptors.<br/>
+            <h1>www.veuanoia.cat</h1></p>
+        </div>
+    </div>
+  </div>
+  <footer>
+      <ul>
+        <li><a href="">Política de privacitat</li></a>
+        <li><a href="">Política de cookies</li></a>
+        <li>App desenvolupada per <a href="https://wwww.bit-data.es">Bit_Data</li></a>
+      </ul>
+  </footer>
+</body>
+</html>
