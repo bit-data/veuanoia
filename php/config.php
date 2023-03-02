503c9fa23f6315ee49249de3d67354333b7d76a4
albertescobar <119687108+albertescobar@users.noreply.github.com>
2023-03-02 12:20:06 +0100

    Update new bbdd

--- php/config.php
+++ php/config.php
@@ -1,8 +1,8 @@
 <?php
-define('USER', 'ddb195589');
+define('USER', 'ddb199657');
 define('PASSWORD', '6ce.BM*u@VI)');
-define('HOST', 'bbdd.ambiens.es');
-define('DATABASE', 'ddb195589');
+define('HOST', 'bbdd.clubveuanoia.es');
+define('DATABASE', 'ddb199657');
 
 try {
     $connection = new PDO("mysql:host=".HOST.";dbname=".DATABASE, USER, PASSWORD);
