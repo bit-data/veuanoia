ff21a2dc7a2607f70c2f273c838a94963880272b
albertescobar <119687108+albertescobar@users.noreply.github.com>
2023-03-10 20:50:32 +0100

    poves 5 odoo php8.0

--- odoo2/base.php
+++ odoo2/base.php
@@ -1,8 +1,6 @@
 <?php
 
-// Include the Ripcord library
-
-require_once('ripcord/ripcord.php');
+require_once('phpxmlrpc-4.10.1/lib/xmlrpc.inc');
 include('../php/config.php');
 
 // Create connection
@@ -26,134 +24,141 @@ $username = "club@veuanoia.cat";
 $password = "VeuAnoia2022";
 
 // Create a new Ripcord client
-$common = ripcord::client("$url/xmlrpc/2/common");
+$client = new xmlrpc_client("$url/xmlrpc/2/common");
 
-// Authenticate
-$uid = $common->authenticate($db, $username, $password, array());
+// Crear una solicitud XML-RPC para la función "authenticate"
+$request = new xmlrpcmsg('authenticate', array(
+    new xmlrpcval($db, 'string'),
+    new xmlrpcval($username, 'string'),
+    new xmlrpcval($password, 'string'),
+    new xmlrpcval(array(), 'struct')
+));
 
-// Create a new client for the objects API
-$models = ripcord::client("$url/xmlrpc/2/object");
+// Enviar la solicitud al servidor y obtener la respuesta
+$response = $client->send($request);
 
-//---->END CONNEXIÓ<--------
+// Obtener el UID del usuario autenticado de la respuesta
+$uid = $response->value()->scalarval();
+
+echo 'hola '.$uid;
+
+// Crear un objeto cliente XML-RPC para el objeto "object"
+$client = new xmlrpc_client("$url/xmlrpc/2/object");
 
 
-$sales = $models->execute_kw($db, $uid, $password,'sale.subscription', 'search_read', [[]], ['fields' => ['name','partner_id','stage_id']]); //sql 500 registres unique cif
-//$sales = $models->execute_kw($db, $uid, $password,'sale.subscription', 'search_read', [[['name', '=', 'VEU05']]], ['fields' => ['name','partner_id','stage_id']]);
-//$sales = $models->execute_kw($db, $uid, $password,'sale.subscription', 'search_read', [[['name', 'like', 'VEU%']]], ['fields' => ['name','partner_id','stage_id']]); //sql 497 registres unique cif
+// Crear una solicitud XML-RPC para la función "execute_kw"
+/*$request = new xmlrpcmsg('execute_kw', array(
+   new xmlrpcval($db, 'string'),
+   new xmlrpcval($uid, 'int'),
+   new xmlrpcval($password, 'string'),
+   new xmlrpcval('res.partner', 'string'),
+   new xmlrpcval('search_read', 'string'),
+   new xmlrpcval(
+      array(
+         new xmlrpcval(array(), 'array', 'struct'),
+         new xmlrpcval(
+            array(
+               new xmlrpcval('name', 'string'),
+               new xmlrpcval('email', 'string'),
+               new xmlrpcval('mobile', 'string'),
+               new xmlrpcval('vat', 'string')
+            ), 'array'
+         )
+      ), 'array'
+   )
+));*/
+$id_partner = 19600; // the ID of the partner you want to filter by
 
 
-foreach ($sales as $sale) {
-    $codi_subscriptor = $sale['name'];
-    echo "Codi subscriptor: " . $codi_subscriptor . "<br>";
-    //echo "Codi subscriptor: " . $sale['name'] . "<br>";
+$partner_id = 196000;
+$id_list[0]= new xmlrpcval($partner_id, 'int');
 
-    $nom_subcriptor = $sale['partner_id'][1];
-    echo "Nom: " . $nom_subcriptor . "<br>";
-    //echo "Nom: " . $sale['partner_id'][1] . "<br>";
+$request = new xmlrpcmsg('execute_kw', array(
+   new xmlrpcval($db, 'string'),
+   new xmlrpcval($uid, 'int'),
+   new xmlrpcval($password, 'string'),
+   new xmlrpcval('res.partner', 'string'),
+   new xmlrpcval('search_read', 'string'),
+   new xmlrpcval($id_list, "struct"), 
+   new xmlrpcval(
+      array(
+         new xmlrpcval(array(), 'array', 'struct'),
+         new xmlrpcval(
+            array(
+               new xmlrpcval('name', 'string'),
+               new xmlrpcval('email', 'string'),
+               new xmlrpcval('mobile', 'string'),
+               new xmlrpcval('vat', 'string'),
+            ), 'array'
+         )
+      ), 'array'
+   )
+));
 
-    $estat_subscripcio = $sale['stage_id'][1];
-    echo "Estat subscripció: " . $estat_subscripcio . "<br>";
-    //echo "Estat subscripció: " . $sale['stage_id'][1] . "<br>";
+/*$id_partner = 123; // the ID of the partner you want to filter by
 
-    //id client a la taula subscriptor per agafar les dades dela taula client
-    $partner_id = $sale['partner_id'][0];
-    echo "id_client ".$partner_id. "<br>";
-    $partners = $models->execute_kw($db, $uid, $password,'res.partner', 'search_read', [[['id', '=', $partner_id]]], ['fields' => ['name', 'email', 'mobile','phone','vat']]);
-    foreach ($partners as $partner) {
+$request = new xmlrpcmsg('execute_kw', array(
+   new xmlrpcval($db, 'string'),
+   new xmlrpcval($uid, 'int'),
+   new xmlrpcval($password, 'string'),
+   new xmlrpcval('res.partner', 'string'),
+   new xmlrpcval('search_read', 'string'),
+   new xmlrpcval(
+      array(
+         new xmlrpcval(
+            array(
+               array('id', '=', $id_partner)
+            ), 'array'
+         ),
+         new xmlrpcval(
+            array(
+               new xmlrpcval('name', 'string'),
+               new xmlrpcval('email', 'string'),
+               new xmlrpcval('mobile', 'string'),
+               new xmlrpcval('vat', 'string')
+            ), 'array'
+         )
+      ), 'array'
+   )
+));*/
 
-    //exemple per mostra camp i per comparar nom amb la taula subscriptor per assegurar q es el mateix
-    echo "Nom: " . $partner['name'] . "<br>";
 
-    $email_subscriptor = $partner['email'];
-    echo "Email: " . $email_subscriptor . "<br>";
 
-    $mobil_subscriptor = $partner['mobile'];
-    echo "mòbil: " . $mobil_subscriptor . "<br>";
 
-    $telefon_subscriptor = $partner['phone'];
-    echo "phone: " . $telefon_subscriptor . "<br>";
 
-    $cif_subscriptor = $partner['vat'];
-    echo "CIF: " . $cif_subscriptor . "<br>";
 
-    echo "<br>";
 
-//bucle per detctar si esta acabada la subscripció
-    }
-    if (($estat_subscripcio == "In Progress")){
 
-      //$sql = "SELECT * FROM subscriptors_odoo";
-        $sql = "INSERT INTO subscriptors_odoo VALUES ('','$codi_subscriptor',$partner_id,'$nom_subcriptor','$cif_subscriptor','$email_subscriptor','$telefon_subscriptor','$mobil_subscriptor')";
-        $sql2 = "INSERT INTO subscriptors_passwords VALUES ($partner_id,'$cif_subscriptor','$cif_subscriptor')";
+echo 'hola2 '.$uid;
 
-        $stmt->bindParam(':num_subs', $codi_subscriptor, PDO::PARAM_STR);
-        $stmt->bindParam(':id_partern', $partner_id, PDO::PARAM_STR);
-        $stmt->bindParam(':nom', $nom_subcriptor, PDO::PARAM_STR);
-        $stmt->bindParam(':dni', $cif_subscriptor, PDO::PARAM_STR);
-        $stmt->bindParam(':email', $email_subscriptor, PDO::PARAM_STR);
-        $stmt->bindParam(':telefon', $telefon_subscriptor, PDO::PARAM_STR);
-        $stmt->bindParam(':mobil', $mobil_subscriptor, PDO::PARAM_STR);
 
-        $stmt->execute();
+// Enviar la solicitud al servidor y obtener la respuesta
+$response2 = $client->send($request);
 
-        $stmt2->bindParam(':id_partern', $partner_id, PDO::PARAM_STR);
-        $stmt2->bindParam(':dni', $cif_subscriptor, PDO::PARAM_STR);
-        $stmt2->bindParam(':password', $cif_subscriptor, PDO::PARAM_STR);
+// Verificar que la respuesta del servidor es válida
+if (!$response2->faultCode()) {
+   // Obtener los resultados de la respuesta
+   $value = $response2->value();
+   if ($value->kindOf() == 'array') {
+      $results = $value->scalarval();
+      foreach ($results as $result) {
 
-        $stmt2->execute();
+         $codi_subscriptor = $result['name']->scalarval();
+         echo "Codi subscriptor: " . $codi_subscriptor . "<br>";
 
-    $connection = null;
-  }
+         $nom_subcriptor = $result['email']->scalarval();
+         echo "Nom: " . $nom_subcriptor . "<br>";
+
+         $estat_subscripcio = $result['mobile']->scalarval();
+         echo "Estat subscripció: " . $estat_subscripcio . "<br>";
+         // Hacer algo con los resultados
+
+         // Obtener el ID del partner
+         $partner_id = $result['vat']->scalarval();
+         echo "vat " . $partner_id. "<br>";
+}
+}
 }
 
-//---->Codi aprofitable<--------
-
-//Per buscar un id de subscriptor
-/*
-$sales_order_id = 627;
-//$sales_order_data = $models->execute_kw($db, $uid, $password, 'sale.order', 'read', array(array($sales_order_id)));
-$sales_order_data = $models->execute_kw($db, $uid, $password, 'sale.subscription', 'read', array(array($sales_order_id)));
-//print_r($sales_order_data);
-echo '<br>';
-//per llistar els camps concrets que ens interessen
-echo 'codi subscriptor: '.$sales_order_data[0]['name'].'<br>';
-echo 'nom: '.$sales_order_data[0]['partner_id'][1].'<br>';
-//echo $sales_order_data[0]['display_name'].'<br>';
-//echo $sales_order_data[0]['x_nom_id'].'<br>';
-//echo 'vàlid fins: '.$sales_order_data[0]['recurring_next_date'].'<br>';
-echo 'Subcripció?: '.$sales_order_data[0]['stage_id'][1].'<br>';
-//per saber el id client i consultar les dades de client
-$partner_id = $sales_order_data[0]['partner_id'][0];
-echo 'id taula client: '.$partner_id.'<br>';
-
-$partners_dades = $models->execute_kw($db, $uid, $password,'res.partner', 'search_read', [[['id', '=', $partner_id]]], ['fields' => ['name', 'email', 'mobile','phone','vat']]);
-
-foreach ($partners_dades as $partner) {
-    echo "Nom: " . $partner['name'] . "<br>";
-    echo "Email: " . $partner['email'] . "<br>";
-    echo "mòbil: " . $partner['mobile'] . "<br>";
-    echo "phone: " . $partner['phone'] . "<br>";
-    echo "CIF: " . $partner['vat'] . "<br>";
-    echo "<br>";
-}*/
-// per llistar les dades concretes de tots els clients
-/*$partners = $models->execute_kw($db, $uid, $password,'res.partner', 'search_read', [[]], ['fields' => ['name', 'email', 'mobile','vat']]);
-
-foreach ($partners as $partner) {
-    echo "Nombre: " . $partner['name'] . "<br>";
-    echo "Email: " . $partner['email'] . "<br>";
-    echo "mobile: " . $partner['mobile'] . "<br>";
-    echo "CIF: " . $partner['vat'] . "<br>";
-    echo "<br>";
-}*/
-
-/*//no tenim permisos per veure les taules
-$tables = $models->execute_kw($db, $uid, $password,
-       'ir.model', 'search',
-       array(array(array('model', '=', 'res.partner'))));
-   foreach ($tables as $table) {
-       echo $table . "\n";
-   }
-*/
 
 ?>
