0ff7b7def6ad11e729f9a41706b5d4157b092f4b
albertescobar <119687108+albertescobar@users.noreply.github.com>
2023-03-23 17:18:30 +0100

    Actualització de dondomino a dinahosting

--- php/config.php
+++ php/config.php
@@ -1,8 +1,13 @@
 <?php
-define('USER', 'ddb199657');
+/*define('USER', 'ddb199657');
 define('PASSWORD', '^]9X!#@-NB1w');
 define('HOST', 'bbdd.clubveuanoia.es');
-define('DATABASE', 'ddb199657');
+define('DATABASE', 'ddb199657');*/
+
+define('USER', 'bit_data');
+define('PASSWORD', 'F6c9P545[0|#');
+define('HOST', 'hl1119.dinaserver.com');
+define('DATABASE', 'clubveuanoia');
 
 try {
     $connection = new PDO("mysql:host=".HOST.";dbname=".DATABASE, USER, PASSWORD);
