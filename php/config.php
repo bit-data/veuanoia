268d3e1f0a91890b217143596466d95272f8d183
albertescobar <119687108+albertescobar@users.noreply.github.com>
2023-06-15 11:11:34 +0200

    Afegit set name utf8 per mostrar bé els accents

--- php/config.php
+++ php/config.php
@@ -10,7 +10,9 @@ define('HOST', 'hl1119.dinaserver.com');
 define('DATABASE', 'clubveuanoia');
 
 try {
-    $connection = new PDO("mysql:host=".HOST.";dbname=".DATABASE, USER, PASSWORD);
+    $connection = new PDO("mysql:host=".HOST.";dbname=".DATABASE, USER, PASSWORD,
+    array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+    //$connection->set_charset("utf8");
 } catch (PDOException $e) {
     exit("Error: " . $e->getMessage());
 }//echo "Connected successfully \n";
