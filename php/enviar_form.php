0684e2847f16dfe07749181e3d69ae1b9afc1568
albertescobar <119687108+albertescobar@users.noreply.github.com>
2023-11-01 13:59:31 +0100

    Correcció enviament email

--- php/enviar_form.php
+++ php/enviar_form.php
@@ -1,8 +1,17 @@
 <?php
+
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+require 'PHPMailer/src/Exception.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
 if(isset($_POST['email'])) {
 
 //email i subject
-$email_to = "club@veuanoia.cat";
+$email_to = "info@bit-data.es";
 $email_subject = "App club subscriptor";
 
 // Validació camps formulari
@@ -16,24 +25,48 @@ header("Location: email_no_enviat.html");
 die();
 }
 
-//Cos email
-$email_message = "Informació del formulari de contacte:\n\n";
-$email_message .= "Subscriptor/a: " . $_POST['num_sub'] . "\n";
-$email_message .= "Nom: " . $_POST['nom_cognoms'] . "\n";
-$email_message .= "E-mail: " . $_POST['email'] . "\n";
-$email_message .= "Mòbil: " . $_POST['mobil'] . "\n";
-$email_message .= "Telèfon: " . $_POST['telefon'] . "\n";
-$email_message .= "Comentaris: " . $_POST['comentari'] . "\n\n";
+// Crea una nova instància de PHPMailer
+$mail = new PHPMailer(true);
+//Codificació de caràcters
+$mail->CharSet = 'UTF-8';
 
-// Enviament de l'email
-$email_from = $_POST['email'];
-$headers = 'From: '.$email_from."\r\n".
-'Reply-To: '.$email_from."\r\n" .
-'X-Mailer: PHP/' . phpversion();
-@mail($email_to, $email_subject, $email_message, $headers);
+try {
+    // Configuració del servidor SMTP i les credencials
+    $mail->isSMTP();
+    $mail->Host = 'bit-data-es.correoseguro.dinaserver.com'; // Servidor SMTP
+    $mail->SMTPAuth = true;
+    $mail->Username = $email_to; // Email que rep
+    $mail->Password = '*Bit-Data19'; // Contrasenya email
+    $mail->SMTPSecure = 'tls';
+    $mail->Port = 587;
 
-//missatge d'enviament correcte
-//echo "¡El formulario se ha enviado con éxito!";
-header("Location: email_enviat.html");
+    // Configuració remitent i destinatari
+    $mail->setFrom($_POST['email'], $_POST['nom_cognoms']);
+    $mail->addAddress($email_to, 'VEU ANOIA');
+
+    // Configura el títol i el cos del email
+    $mail->isHTML(true);
+    $mail->Subject = $email_subject;
+
+    $email_message = "Informació del formulari de contacte:<br><br>";
+    $email_message .= "Subscriptor/a: " . $_POST['num_sub'] . "<br>";
+    $email_message .= "Nom: " . $_POST['nom_cognoms'] . "<br>";
+    $email_message .= "E-mail: " . $_POST['email'] . "<br>";
+    $email_message .= "Mòbil: " . $_POST['mobil'] . "<br>";
+    $email_message .= "Telèfon: " . $_POST['telefon'] . "<br>";
+    $email_message .= "Comentaris: " . $_POST['comentari'] . "<br><br>";
+
+    $mail->Body = $email_message;
+
+
+    // Envia el correo
+    $mail->send();
+    //echo 'l'email s'ha enviat correctament.';
+    header("Location: email_enviat.html");
+} catch (Exception $e) {
+    //echo 'Error al enviar l'email: ' . $mail->ErrorInfo;
+    header("Location: email_no_enviat.html");
+}
+
 }
 ?>
