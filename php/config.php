b15d7f43289f2433f1e5f17c82ba1e74de8f243a
albertescobar <albert.escobar@bit-data.es>
2025-05-27 18:25:55 +0200

    canvi connexio BBDD

--- php/config.php
+++ php/config.php
@@ -1,13 +1,14 @@
 <?php
-/*define('USER', 'ddb199657');
-define('PASSWORD', '^]9X!#@-NB1w');
-define('HOST', 'bbdd.clubveuanoia.es');
-define('DATABASE', 'ddb199657');*/
 
-define('USER', 'bit_data');
+/*define('USER', 'bit_data');
 define('PASSWORD', 'F6c9P545[0|#');
 define('HOST', 'hl1119.dinaserver.com');
-define('DATABASE', 'clubveuanoia');
+define('DATABASE', 'clubveuanoia');*/
+
+define('USER', 'ddb253515');
+define('PASSWORD', 'F6c9P545[0|#');
+define('HOST', 'bbdd.ambiens.es');
+define('DATABASE', 'ddb253515');
 
 try {
     $connection = new PDO("mysql:host=".HOST.";dbname=".DATABASE, USER, PASSWORD,
